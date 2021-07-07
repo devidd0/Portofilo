eb5c9cd8ebdb31f2a3e85a927ed3d654cface055
IamEziz <i.am.hacker.eziz@gmail.com>
2021-07-07 19:46:39 +0400

    MyPortofilio web page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,6 +3,7 @@ import { VscGithub } from "react-icons/vsc";
 import { AiOutlineYoutube } from "react-icons/ai";
 import { FiInstagram } from "react-icons/fi";
 import Particles from "react-particles-js";
+console.log('hi my name is kyle')
 import TypewriterComponent from "typewriter-effect";
 // import Swiper core and required modules
 import SwiperCore, {
