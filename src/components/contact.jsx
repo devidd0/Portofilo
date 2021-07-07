2ed05b3a5657566c28836436a65d08b51d205509
IamEziz <i.am.hacker.eziz@gmail.com>
2021-07-07 19:52:43 +0400

    Final version 1.0.1

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,8 +3,6 @@ import { VscGithub } from "react-icons/vsc";
 import { AiOutlineYoutube } from "react-icons/ai";
 import { FiInstagram } from "react-icons/fi";
 import Particles from "react-particles-js";
-console.log('hi my name is kyle')
-console.log('ohooo')
 import TypewriterComponent from "typewriter-effect";
 // import Swiper core and required modules
 import SwiperCore, {
